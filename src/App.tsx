fe381e5629cd08693608be95a8f963c189e9c9de
Cleyton Caminha <cleytoncaminha01@gmail.com>
2022-03-22 23:12:28 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { getMovies, searchMovies } from './service';
 
 function App() {
+  const ImageUrl: string = "https://image.tmdb.org/t/p/w300"
+
+  const [initialList, setInitialList] = useState<any>([])
+  const [numberPage, setNumberPage] = useState<number>(1)
+  const [search, setSearch] = useState<string>("")
+  const [filteredList, setFilteredList] = useState<any>([])
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value)
+    filterListMovies(event.target.value)
+  }
+
+  const filterListMovies = (search: string) => {
+    if (search === "") return;
+
+    async function fetchData() {
+      const moviesFilters: any = await searchMovies(search)
+      setFilteredList(moviesFilters)
+
+    }
+
+    fetchData()
+  }
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const response: any = await getMovies(numberPage)
+      setInitialList(response)
+
+    }
+
+    fetchData()
+
+  }, [numberPage])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form>
+          <div>
+            <label htmlFor="search">Procure seu filme favorito </label>
+            <input type="text" name="search" id="search" onChange={handleInputChange} value={search} />
+          </div>
+        </form>
+        {numberPage > 1 ? <button type="button" onClick={() => { setNumberPage(numberPage - 1) }}>pagina anterior</button> : ""}
+        <h1>{numberPage}</h1>
+        {numberPage < 6 ? <button type="button" onClick={() => { setNumberPage(numberPage + 1) }}>proxima pagina</button> : ""}
+      </div>
+      {search !== "" ? filteredList.map((item: any, index: number) => {
+        return (
+          <div key={index}>
+            <h1>{item.title}</h1>
+            <img src={ImageUrl + item.poster_path}></img>
+          </div>
+        )
+      }) : initialList.map((item: any, index: number) => {
+        return (
+          <div key={index}>
+            <h1>{item.title}</h1>
+            <img src={ImageUrl + item.poster_path} ></img>
+          </div>
+        )
+      })}
+
+
     </div>
   );
 }
---
+++ src/service/index.tsx
@@ -0,0 +1,15 @@
+const ApiKey: string = "&api_key=b8631ce6585f8712833dab184716894b"
+const BaseUrl: string = "https://api.themoviedb.org/3"
+const ApiUrl: string = BaseUrl + "/discover/movie?sort_by=popularity.desc&language=pt-BR&page="
+
+export const getMovies = async function (page: number = 1) {
+    const response = await fetch(`${ApiUrl + page + ApiKey}`);
+    const json = await response.json();
+    return json.results
+}
+
+export const searchMovies = async function (query:string) {
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=b8631ce6585f8712833dab184716894b&language=pt-BR&query=${query}`);
+    const json = await response.json();
+    return json.results
+}
